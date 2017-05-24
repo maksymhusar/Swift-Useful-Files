006f18a766893ebda18912dd66de786f31ade77d
Azurelus <mkgusar@gmail.com>
2017-05-24 11:45:11 +0300

    Create GlobalAppearance_Example.swift

---
+++ Examples/GlobalAppearance_Example.swift
@@ -0,0 +1,9 @@
+// GlobalAppearance.swift used
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        ...
+        GlobalAppearance.configure()
+        return true
+    }
