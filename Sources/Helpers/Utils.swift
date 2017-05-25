81ab9cac68e013aaa6583d3822ed49cf77532442
Azurelus <mkgusar@gmail.com>
2017-05-25 11:45:51 +0300

    Update Utils.swift

--- Sources/Helpers/Utils.swift
+++ Sources/Helpers/Utils.swift
@@ -26,34 +26,4 @@ struct Utils {
             return 1.0
         }
     }()
-    
-    // better to move to separate file
-    static func cropImage(_ image: UIImage, width: CGFloat) -> UIImage {
-        var imageWidth = image.size.width
-        var imageHeight = image.size.height
-        
-        if imageWidth > width || imageHeight > width {
-            if imageWidth > imageHeight {
-                let scale = width / imageWidth
-                imageWidth = width
-                imageHeight = imageHeight * scale
-            } else {
-                let scale = width / imageHeight
-                imageHeight = width
-                imageWidth = imageWidth * scale
-            }
-        }
-        
-        let newWidth = CGFloat(imageWidth)
-        let newHeight = CGFloat(imageHeight)
-        
-        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
-        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        guard let croppedImage = newImage else {
-            fatalError("Error: Could not crop image")
-        }
-        return croppedImage
-    }
 }
