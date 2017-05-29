a4f58670a1b1dd6f29814e0f69a3ed3e132a5997
Azurelus <mkgusar@gmail.com>
2017-05-29 09:40:11 +0300

    Add files via upload

---
+++ Scripts/swiftTODOScript.swift
@@ -0,0 +1,3 @@
+TAGS="TODO:|FIXME:"
+find "${SRCROOT}" \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
+
