da3b96a565115d1d3f3639efb369dd2565c08b52
Maksym Husar <mkgusar@gmail.com>
2017-12-21 16:12:01 +0200

    Update swiftTODOScript.swift
    Update

--- Scripts/swiftTODOScript.swift
+++ Scripts/swiftTODOScript.swift
@@ -1,3 +1,2 @@
 TAGS="TODO:|FIXME:"
-find "${SRCROOT}" \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
-
+find "." \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -not -path "./Pods/*" -not -path "./Scripts/*" -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
