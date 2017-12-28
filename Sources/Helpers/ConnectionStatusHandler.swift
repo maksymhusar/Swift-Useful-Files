5f15741d8339deee3d73fcc11a4e83abcd3b90bf
Maksym Husar <mkgusar@gmail.com>
2017-12-28 15:01:34 +0200

    Create ConnectionStatusHandler.swift

---
+++ Sources/Helpers/ConnectionStatusHandler.swift
@@ -0,0 +1,45 @@
+//
+//  ConnectionStatusHandler.swift
+//  Swift-Useful-Files
+//
+//  Created by Maksym Husar on 12/28/17.
+//  Copyright © 2017 Maksym Husar. All rights reserved.
+//
+
+import Foundation
+import PKHUD
+
+class ConnectionStatusHandler {
+    static let instance = ConnectionStatusHandler()
+    private init() { }
+    
+    // MARK: - Public methods
+    func startHandling() {
+        addNotifications()
+    }
+    
+    func stopHandling() {
+        removeNotifications()
+    }
+    
+    // MARK: - Private methods
+    private func addNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(internetNotAvailable(_:)), name: .InternetNotAvailable, object: nil)
+    }
+    
+    private func removeNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func internetNotAvailable(_ notification: Notification) {
+        if let currentVC = UIApplication.topViewController(), currentVC is UIAlertController == false {
+            HUD.hide()
+            let title = "No Internet Connection"
+            let message = "Make sure your device is connected to the Internet"
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: { _ in })
+            alert.addAction(cancelAction)
+            currentVC.present(alert, animated: true, completion: nil)
+        }
+    }
+}
